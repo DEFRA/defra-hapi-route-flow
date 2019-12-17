e029c159e31764f5eb4c33dfe4438a44dd0a6b18
ben <ben@surgison.net>
2019-12-17 10:40:30 +0000

    Clean up code

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 const { RouteFlowEngine } = require('route-flow-engine')
 
-  const register = async (server, { flowConfig: config, handlersDir }) => {
-    const createRoutes = async (node) => {
-      const Handlers = require(`${handlersDir}/${node.handlers}`)
-      const handlers = node.handlers = new Handlers()
+const register = async (server, { flowConfig: config, handlersDir }) => {
+  const createRoutes = async (node) => {
+    const Handlers = require(`${handlersDir}/${node.handlers}`)
+    const handlers = node.handlers = new Handlers()
 
-      if (handlers.getPayload) {
-        handlers.payload = await handlers.getPayload()
-      }
-
-      if (node.next) {
-        handlers.getNextPath = async (...args) => {
-          const nextNode = await node.next(...args)
-          return nextNode.path
-        }
-      }
-
-      if (node.title) {
-        handlers.getPageHeading = async (...args) => {
-          return node.title(...args)
-        }
-      }
-
-      const { path, isQuestionPage = false, view, tags = [] } = node
-      const routes = handlers.routes({
-        path,
-        app: {
-          view,
-          isQuestionPage,
-          tags,
-        },
-      })
-
-      routes.forEach((route) => server.route(route))
+    if (handlers.getPayload) {
+      handlers.payload = await handlers.getPayload()
     }
-    const resolveQuery = async (routes, query, ...args) => {
-      return routes.handlers[query](...args)
+
+    if (node.next) {
+      handlers.getNextPath = async (...args) => {
+        const nextNode = await node.next(...args)
+        return nextNode.path
+      }
     }
-    const routeFlowEngine = new RouteFlowEngine({ config, createRoutes, resolveQuery })
+
+    if (node.title) {
+      handlers.getPageHeading = async (...args) => {
+        return node.title(...args)
+      }
+    }
+
+    const { path, isQuestionPage = false, view, tags = [] } = node
+    const routes = handlers.routes({
+      path,
+      app: {
+        view,
+        isQuestionPage,
+        tags
+      }
+    })
+
+    routes.forEach((route) => server.route(route))
   }
+
+  const resolveQuery = async (routes, query, ...args) => {
+    return routes.handlers[query](...args)
+  }
+
+  return new RouteFlowEngine({ config, createRoutes, resolveQuery })
+}
 
 exports.plugin = {
   name: 'defra-hapi-route-flow',
@@ -46,5 +48,3 @@ exports.plugin = {
   once: true,
   pkg: require('./package.json')
 }
-
-

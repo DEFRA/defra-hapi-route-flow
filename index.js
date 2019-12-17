74de2a3fc261dd44e87535d565b14185a0afadea
ben <ben@ajaxscape.com>
2019-12-17 17:54:53 +0000

    Expose createRoutes for test purposes

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
 const { RouteFlowEngine } = require('route-flow-engine')
 const pkg = require('./package.json')
 
+const createRoutes = async (node, server, handlersDir) => {
+  const Handlers = require(`${handlersDir}/${node.handlers}`)
+  Handlers.server = server
+
+  const handlers = node.handlers = new Handlers()
+
+  if (handlers.getPayload) {
+    handlers.payload = await handlers.getPayload()
+  }
+
+  if (node.next) {
+    handlers.getNextPath = async (...args) => {
+      const nextNode = await node.next(...args)
+      return nextNode.path
+    }
+  }
+
+  if (node.title) {
+    handlers.getPageHeading = async (...args) => {
+      return node.title(...args)
+    }
+  }
+
+  const { path, isQuestionPage = false, view, tags = [] } = node
+  const routes = handlers.routes({
+    path,
+    app: {
+      view,
+      isQuestionPage,
+      tags
+    }
+  })
+
+  routes.forEach((route) => server.route(route))
+}
+
 const register = async (server, { flowConfig: config, handlersDir }) => {
   server.app.flow = async (routeId) => RouteFlowEngine.flow(routeId)
-
-  const createRoutes = async (node) => {
-    const Handlers = require(`${handlersDir}/${node.handlers}`)
-    Handlers.server = server
-
-    const handlers = node.handlers = new Handlers()
-
-    if (handlers.getPayload) {
-      handlers.payload = await handlers.getPayload()
-    }
-
-    if (node.next) {
-      handlers.getNextPath = async (...args) => {
-        const nextNode = await node.next(...args)
-        return nextNode.path
-      }
-    }
-
-    if (node.title) {
-      handlers.getPageHeading = async (...args) => {
-        return node.title(...args)
-      }
-    }
-
-    const { path, isQuestionPage = false, view, tags = [] } = node
-    const routes = handlers.routes({
-      path,
-      app: {
-        view,
-        isQuestionPage,
-        tags
-      }
-    })
-
-    routes.forEach((route) => server.route(route))
-  }
 
   const resolveQuery = async (routes, query, ...args) => {
     return routes.handlers[query](...args)
   }
 
-  return new RouteFlowEngine({ config, createRoutes, resolveQuery })
+  const _createRoutes = async (node) => {
+    return createRoutes(node, server, handlersDir)
+  }
+
+  return new RouteFlowEngine({ config, _createRoutes, resolveQuery })
 }
 
 exports.plugin = {
@@ -53,3 +57,5 @@ exports.plugin = {
   once: true,
   pkg
 }
+
+exports.test = { createRoutes }

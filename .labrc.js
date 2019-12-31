679d345058ed064efe59b99f1164bb8397bb157f
ben <ben@surgison.net>
2019-12-31 18:47:54 +0000

    Add unit tests

--- .labrc.js
+++ .labrc.js
@@ -1,10 +1,11 @@
 const glob = require("glob")
 const testFiles = glob.sync('lib/**/*.test.js')
+const testHandlers = glob.sync('lib/test-handlers/*.handlers.js')
 const config = {
   paths: testFiles,
   coverage: true,
   threshold: 85,
-  'coverage-exclude': testFiles,
+  'coverage-exclude': [...testHandlers, ...testFiles],
   globals: '__core-js_shared__'
 }
 
